f1d0c5c0896eb6dbecd11cb45d8f7d56080d1074
skrall3119 <ajanci429@students.mchenry.edu>
2017-02-14 15:47:31 -0600

    Update Practice 4.3.py

--- Practice 4.3.py
+++ Practice 4.3.py
@@ -1,4 +1,12 @@
-years = int(input("How many years of data do you have?"))
-for yearCount in range(years):
-    for monthCount in range(12):
-        inchesOfRain = input("Number of inches in month", monthCount, "of year", yearCount, ":")
+months = 0
+totalRain = 0
+years = int(input("plase enter the number of years: "))
+
+for currentYear in range(1, years + 1):
+    for currentMonth in range( 1,13 ):
+        monthlyRain = float(input("Please type the inches of " + "rainfall for month "+format(currentMonth, "d") + " year "+ format(currentYear,"d")+ ":"))
+        totalRain += monthlyRain
+        months += 1
+averageRainfall = totalRain / months
+
+print("\nNumber of months:", format(months,"d"), "Total inches of rainfall:", format(totalRain, ".2f"), "average rainfall:", format(averageRainfall, ".2f"))
