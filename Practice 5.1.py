59797a1d3c4cbd681c4e10433a8d4da811cd2d09
skrall3119 <ajanci429@students.mchenry.edu>
2017-02-14 19:35:27 -0600

    Update Practice 5.1.py

--- Practice 5.1.py
+++ Practice 5.1.py
@@ -1,11 +1,11 @@
-def main():
+def main(): #the main function that prints the lowest reccomended insurance payment.
     print("The minimum amount of insurance you should buy is: $" + format(calculateInsurance(getCost()), ',.2f'))
 
-def getCost():
+def getCost(): #gets the value of the building from the user
     cost = float(input("enter cost of building: "))
     return cost
 
-def calculateInsurance(cost):
+def calculateInsurance(cost):# calculates the insurance cost using the value recieved from the user
     insuranceCost = cost * .8
     return insuranceCost
 main()
