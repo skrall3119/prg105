c4590847f4fdd3a083d7a3349a78650058da9e03
skrall3119 <ajanci429@students.mchenry.edu>
2017-02-14 19:39:48 -0600

    Update practice 5.2.py

--- practice 5.2.py
+++ practice 5.2.py
@@ -1,4 +1,4 @@
-def main():
+def main(): #The main function that gets the input from the user and prints out the costs per month and costs per year
     loan = int(input("Enter your monthly loan payment: "))
     insurance = int(input("Enter your monthly insurance payment: "))
     gas = int(input("Enter the amount you spend on gas monthly: "))
@@ -10,15 +10,15 @@ def main():
     printCosts(yearlyCosts, monthlyCosts)
 
 
-def addCosts(a, b, c, d, e):
+def addCosts(a, b, c, d, e): #adds the amounts given by the user
     monthlytotal = a + b + c + d + e
     return int(monthlytotal)
 
-def calculateYearlyCost(monthlytotal):
+def calculateYearlyCost(monthlytotal): #calculates the yearly cost with the value returned by addCosts
     yearlyCost = monthlytotal * 12
     return yearlyCost
 
-def printCosts(yearlyCost, monthlyCost):
+def printCosts(yearlyCost, monthlyCost): #prints the costs calculated in addCosts and calculateYearlyCost
     print("The monthly total for your vehicle is:", format(monthlyCost, ',.2f'))
     print("The yearly total for your vehicle is:", format(yearlyCost, ',.2f'))
 
