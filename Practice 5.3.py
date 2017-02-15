cc9fcbc9813c9d652912056eed9aaee5ae6c21e1
skrall3119 <ajanci429@students.mchenry.edu>
2017-02-14 19:42:50 -0600

    Update Practice 5.3.py

--- Practice 5.3.py
+++ Practice 5.3.py
@@ -1,4 +1,4 @@
-def main():
+def main(): #assigns functions to variables and prints results from the fuctions
     actualValue = getValue()
     assessmentValue = calculateAssessmentValue(actualValue)
     propertyTax = findPropertyTax(assessmentValue)
@@ -6,15 +6,15 @@ def main():
     print("The assessment value of your home is: $" + format(assessmentValue, ',.2f'))
     print("The property tax on your home is: $" + format(propertyTax, ',.2f'))
 
-def getValue():
+def getValue(): #asks the user for input 
     actualValue = float(input("Enter the actual value of your property. "))
     return actualValue
 
-def calculateAssessmentValue(houseValue):
+def calculateAssessmentValue(houseValue): #calculates the assessment value using the value provided by the user
     assessmentValue = houseValue * .6
     return assessmentValue
 
-def findPropertyTax(assessmentValue):
+def findPropertyTax(assessmentValue): #calculates property tax based on the value of calculateAssessmentValue
     x =  assessmentValue / 100
     propertyTax = .72 * x
     return propertyTax
