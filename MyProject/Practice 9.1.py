e048a156dedbd8a6b395f2c3eedc2b88a14adf0e
skrall3119 <ajanci429@students.mchenry.edu>
2017-04-13 19:48:49 -0500

    Update Practice 9.1.py

--- MyProject/Practice 9.1.py
+++ MyProject/Practice 9.1.py
@@ -1,40 +1,58 @@
 import pickle
 
 
-def add(dic):
+def add(dic):           # adds a new entry to the dictionary
     name = input('Enter the name you wish to add: ')
     email = input('Enter the email you wish to associate with this name: ')
     dic[name] = email
     print(dictionary)
 
 
-def remove(dic):
+def remove(dic):        # removes the entry of the dictionary
     name = input('Enter the name of the person you want removed from your emails: ')
     del dic[name]
     print(dictionary)
 
 
-def change(dic):
+def change(dic):       # allows the user to change the name or email when specified. 
     option = input('Do you want to change the name or the email?: ').lower()
     if option == 'name':
         name = input('Enter the name you want to change: ')
         if name not in dic:
             name = input("That name is not in the list, please enter a name in the list. or use the 'add' command to add a \
-            new email address:")
+            new email address\n")
+            while name not in dic:
+                name = input('Enter the name you want to change: ')
         new_name = input('Enter the new name: ')
         dic[new_name] = dic[name]
         del dic[name]
-        print('\n' + dic)
+        print('\n')
+        print(dic)
+    elif option == 'email':
+        name = input('Enter the name of the persons email you want to change: ')
+        if name not in dic:
+            name = input('That name is not in the list, please enter a name in the list, or use the add command to add \
+            a new name and email combo.: ')
+        new_email = input('Enter the new email address: ')
+        dic[name] = new_email
+        print('\n')
+        print(dic)
 
 
-def show_dic(dic):
+def show_dic(dic):      # prints the dictionary
     print(dic)
 
-dictionary = {}
+# beginning of main code. opens the file if it exists or creates a new one otherwise.
+try:
+    dictionary = pickle.load(open('save.p', 'rb'))
+except EOFError:
+    dictionary = {}
+
 print(dictionary)
-commands = 'List of commands: ' + '\n' + 'add' + '\n' + 'remove' + '\n' 'print' + '\n' + 'quit' + '\n'
+commands = 'List of commands: ' + '\n' + 'add' + '\n' + 'remove' + '\n' 'print' + '\n' + 'quit' + '\n' + 'save' + '\n'
 print(commands)
 user = input("Enter a command: ").lower()
+
 while user != 'quit':
     if user == 'add':
         add(dictionary)
@@ -48,3 +66,12 @@ while user != 'quit':
     elif user == 'print':
         print(dictionary)
         user = input('Enter a command: ')
+    elif user == 'save':
+        pickle.dump(dictionary, open('save.p', 'wb'))
+        print('Progress saved!')
+        user = input('Enter a command: ')
+    else:
+        print('Invalid command.' + '\n' + commands)
+        user = input('Enter a command:')
+else:
+    pickle.dump(dictionary, open('save.p', 'wb'))
